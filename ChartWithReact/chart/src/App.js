5e32a6d4d2fcfe1ffa29f709365edd98b09a9c9f
doyeonkim <doyeonkim106@gmail.com>
2022-09-15 21:27:45 +0900

    Chart-only Web

--- ChartWithReact/chart/src/App.js
+++ ChartWithReact/chart/src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Rechart from "./CustomizedDotLineChart";
 
 function App() {
   return (
-    <div className="App">
-
+    <div style={{ width: 600, height: 400 }}>
+      <h1>
+        <center>Show CO2 Concentration</center>
+      </h1>
+      <Rechart />
     </div> 
   );
 }
