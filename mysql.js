62ad4cf4d52d75cd5c6655849dc3f1169ba389cd
doyeonkim <doyeonkim106@gmail.com>
2022-09-21 23:01:36 +0900

    [STYLE] Add Port in Create DB Connection

--- mysql.js
+++ mysql.js
@@ -1,14 +1,15 @@
 const mysql = require('mysql');
 const conn = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '패스워드',
-    database: '데이터베이스 이름'
+    host: '호스트명',
+    port: 포트번호,
+    user: '사용자명',
+    password: '비밀번호',
+    database: '데이터베이스'
 });
 
 conn.connect();
 
-conn.query('SELECT * FROM env', function(error, results, fields) {
+conn.query('SELECT * FROM 테이블 LIMIT 10', function(error, results, fields) {
     if(error) {
         console.log(error);
     }
