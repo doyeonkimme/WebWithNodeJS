0749aaa3b77f8a1bd888f4fdc3c3ade61ad974a0
doyeonkim <doyeonkim106@gmail.com>
2022-10-21 20:18:14 +0900

    [STYLE]

--- db.js
+++ db.js
@@ -2,8 +2,8 @@ const mysql = require('mysql');
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '패스워드',
-    database: '데이터베이스 이름'
+    password: 'Ehdus6692!',
+    database: 'get_info_env'
 });
 
 module.exports = db;
